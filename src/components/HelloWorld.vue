236c1268c212dcdb0211d1188e01e2ec1e8b6c87
rakulagin <koulagin@gmail.com>
2022-04-20 22:35:02 +0400

    add lesson 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,36 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <input placeholder="x" v-model.number="x" >
+<!--    инпут 1-->
+    <input placeholder="x" v-model.number="x">
     <br>
+<!--    инпут 2-->
     <input placeholder="n" v-model.number="n">
     <br>
-    <button @click="calc('sum')">x + n</button>
-    <button @click="calc('sub')">x - n</button>
-    <button @click="calc('multiply')">x * n</button>
-    <button @click="calc('div')">x / n</button>
-    <button @click="calc('exp')">x^n</button>
+<!--    кнопки операндов-->
+    <button v-for="operand in operands" @click="calc(operand)" :key="operand">
+      {{ operand }}
+    </button>
     <p>{{ result }}</p>
+    <div>
+<!--      показать/скрыть экранную клавиатуру-->
+      <input type="checkbox" @click="show=!show">
+      <span>Отобразиь экранную клавиатуру</span>
+      <div v-show="show">
+        <button v-for="(btn, index) in buttons" :key="index" @click="mouseInput(btn)">
+          {{ btn }}
+        </button>
+        <button @click="backspase()">backspase</button>
+        <br>
+<!--        выбор операнда-->
+        <input type="radio" id="x" value="x" v-model="picked">
+        <label for="x">x</label>
+
+        <input type="radio" id="n" value="n" v-model="picked">
+        <label for="n">n</label>
+        <div v-if="picked"> Выбрана переменная {{ picked }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,33 +44,55 @@ export default {
     return {
       result: 0,
       x: '',
-      n: ''
+      n: '',
+      operands: ['+', '-', '*', '/', '^'],
+      buttons: [0,1,2,3,4,5,6,7,8,9],
+      show: true,
+      picked: ''
+
     }
   },
   methods: {
-    // я не очень понял дз, может быть надо было сделать через Вью все операции на кнопках, но я решил так
-    // если неправильно - легко переделаю
-    // по моим меркам это самый короткий код, конструкция switch аналогична по длине, но мне она просто не нравится :)
-    // если не проверять на некорректные значения
+
     calc(operation) {
-      if(!isNaN(this.x) && !isNaN(this.n)) { // проверка на некорректные значения
-        if (operation == 'sum') {
-          this.result = this.x+this.n
-        } else if (operation == 'sub') {
+      if(!isNaN(this.x) && !isNaN(this.n)) {
+        if (operation == '+') {
+          this.result = +this.x+ +this.n
+        } else if (operation == '-') {
           this.result = this.x-this.n
-        } else if (operation == 'multiply') {
+        } else if (operation == '*') {
           this.result = this.x*this.n
-        } else if (operation == 'div') {
+        } else if (operation == '/') {
           if (this.n) { // проверка на ноль
             this.result = Math.ceil(this.x/this.n)
           } else {
             this.result = "Делить на ноль нельзя!"
           }
-        } else if (operation == 'exp') {
+        } else if (operation == '^') {
           return this.result = Math.pow(this.x,this.n)
         }
       } else {
         this.result="Вы ввели не число";
+      }
+    },
+
+    mouseInput(btn) {
+      if(this.picked == 'x') {
+        let str = this.x.toString().concat(btn)
+        this.x = Number(str)
+      } else if (this.picked == 'n') {
+        let str = this.n.toString().concat(btn)
+        this.n = Number(str)
+      }
+    },
+
+    backspase() {
+      if(this.picked == 'x') {
+        const str = this.x.toString().slice(0, -1)
+        this.x = +str
+      } else if (this.picked == 'n') {
+        const str = this.n.toString().slice(0, -1)
+        this.n = +str
       }
     }
   }
