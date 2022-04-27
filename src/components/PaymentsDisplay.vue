1d3bb53d4bebaaf8b04bb2dc02cb9135f47ea644
rakulagin <koulagin@gmail.com>
2022-04-27 20:37:59 +0400

    add lesson 3

---
+++ src/components/PaymentsDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="paymentList">
+   <div class="paymentItem" v-for="(item, index) in items" :key="index">
+     {{ item.date }} {{ item.category }} {{ item.value }}
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentsDisplay",
+  props: {
+    items: {
+      type: Array,
+      default: () =>[]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
